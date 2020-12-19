473e61f09ce376d5a26be8cbd5bc96c99070ef63
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2020-12-19 16:09:20 +0100

    Css for movie card hover

--- src/componenets/MovieCard.js
+++ src/componenets/MovieCard.js
@@ -4,11 +4,15 @@ import MovieRate from "./MovieRate";
 const MovieCard = ({ title, description, rate, children }) => {
   return (
     <div className="MovieCard">
-      <div className="Poster">{children}</div>
+      <div className="Poster">
+        <div className="img">
+          {children}
+          <h3 className="watchTrailer">Watch Trailer</h3>
+        </div>
+      </div>
       <div className="MovieDescrip">
         <h1>{title}</h1>
         <MovieRate rate={rate} setRate={() => {}} />
-        <p>{description}</p>
       </div>
     </div>
   );
